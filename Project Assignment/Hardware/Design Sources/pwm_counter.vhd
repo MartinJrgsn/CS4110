292cd268db5234d0501e1aaff99fd4e727d28610
Nikolai Eidheim <nikolai.e@live.no>
2023-12-07 10:02:26 +0100

    remove unused signal

--- Project Assignment/Hardware/Design Sources/pwm_counter.vhd
+++ Project Assignment/Hardware/Design Sources/pwm_counter.vhd
@@ -75,7 +75,6 @@ begin
                 echo_start <= counter;
                 measuring <= TRUE;
             elsif echo = '0' and measuring then
-                echo_end <= counter;
                 -- Calculate distance for speed of sound in air ~ 343 m/s)
                 measured_distance <= (counter - echo_start) / 5831;
                 measuring <= FALSE;
